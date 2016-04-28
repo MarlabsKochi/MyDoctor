271d54632f974feca6aa293ace74a8cc019e35f3
mathewvivek <mathewecemce@gmail.com>
2016-04-28 14:52:09 +0530

    appointment booking first setup

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -11,9 +11,30 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-    
+    @appointment= Appointment.new(appointment_params)
+    @appointment.patient_id = current_patient.id
+
+    respond_to do |format|
+     if @appointment.save
+       format.html { redirect_to @appointment, notice: 'Appointment was successfully created'}
+       format.json { render :show, status: :created, location: @appointment }
+     else
+       format.html { render :new }
+       format.json { render json: @appointment.errors, status: :unprocessable_entity }
+     end
+    end
   end
 
   def edit
   end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def appointment_params
+      params[:appointment].parse_time_select! :time
+      params[:appointment][:date].to_date
+      params[:appointment][:doctor_id] = params[:doctor_id]
+      params.require(:appointment).permit(:date, :time, :doctor_id, :patient_id)
+    end
 end
