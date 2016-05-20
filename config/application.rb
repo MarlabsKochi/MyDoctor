1164f0f8af17737c5e9c266bc0c74c5bee939779
mathewvivek <mathewecemce@gmail.com>
2016-05-20 18:30:22 +0530

    bootstrap template try

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module RailsDevise
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # add app/assets/fonts to the asset path
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
